92418f3e4b935c09a37b3594945f36e7e8c6e7ef
Abdelghani Marjaoui <marjaoui@orange.fr>
2020-10-28 10:57:05 +0100

    rajout fichier plan de tests

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,15 +1,17 @@
 ////// afficher le montant global et le numéro de commande
 document.getElementById('total4');
-document.getElementById('confirmation4');
-const total = parseInt (localStorage.getItem('total'));  //// attribuer une valeur fixe à la variabe total
-const confirmation = JSON.parse(localStorage.getItem('confirmation'))
-console.log(total, confirmation);
-//total4.append(total);
-//confirmation4.append(confirmation);
-let afficheConfirmation = document.getElementById('confirmation4');
+const total = parseInt (localStorage.getItem('total')); 
 let afficheTotal = document.getElementById('total4');
-afficheConfirmation.textContent = 'Votre commande porte le n°  :' + ' ' + confirmation.orderId;    ////  attribuer une valeure à confirmation.orderId
 afficheTotal.textContent = 'Le montant total de votre commande est de :' + ' ' + total + ' ' + '€';
+
+document.getElementById('confirmation4');
+const confirmation = JSON.parse(localStorage.getItem('confirmation'));
+let afficheConfirmation = document.getElementById('confirmation4');
+afficheConfirmation.textContent = 'Votre commande porte le n°  :' + ' ' + confirmation.orderId;
+console.log(total, confirmation);
+
+    
+
 
 localStorage.clear();
 
